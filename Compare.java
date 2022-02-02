fbfb6384c93df97cf661a376a32851c16e4c89c3
Neal Jamieson <54950355+TheDrakonLord@users.noreply.github.com>
2022-02-02 15:07:56 -0600

    Add comments

--- Compare.java
+++ Compare.java
@@ -1,48 +1,66 @@
 /**
- * 
+ * Group Members: Neal Jamieson, James Gregory, Landdon Squiers
+ * Class: CSCI_3342_002
+ * Assignment: Exploration 1 
  */
 package exploration1;
 
 /**
- * @author Neal Jamieson, James Gregory, Landdon Squiers
- * Class: CSCI_3342_002
- * Assignment: Exploration 1
+ * @author Neal Jamieson
  * 
- *
+ * This class creates the recursive and iterative methods for evaluating the algorithm given
  */
-
-
 public class Compare {
+	//Declare private counter variables
 	private static int _iterativeCount = 0;
 	private static int _recursiveCount = 0;
 	
+	/**
+	 * 
+	 * @return returns the number of times the iterative method has been run
+	 */
 	public static int getIterativeCount() {
 		return _iterativeCount;
 	}
 	
+	/**
+	 * 
+	 * @param a value to set the iterative counter variable to
+	 */
 	public static void setIterativeCount(int a) {
 		_iterativeCount = a;
 	}
 	
+	/**
+	 * 
+	 * @return returns the number of times the recursive method has been run
+	 */
 	public static int getRecursiveCount() {
 		return _recursiveCount;
 	}
 	
+	/**
+	 * 
+	 * @param a value to set the recursive counter variable to
+	 */
 	public static void setRecursiveCount(int a) {
 		_recursiveCount = a;
 	}
 	
-	public Compare() {
-		
-	}
-	
+	/**
+	 * evaluates the algorithm using an iterative method
+	 * @param n the input size to evaluate the algorithm on
+	 * @return returns the result from evaluating the algorithm
+	 */
 	public static int Iterative(int n) {
+		//Declare initial facts
 		_iterativeCount = 0;
 		int nMinus3 = -3;
 		int nMinus2 = 3;
 		int nMinus1 = 2;
 		int currentN = 0;
 		
+		//Check if n is one of the base cases
 		switch(n) {
 			case 0:
 				return nMinus3;
@@ -51,24 +69,35 @@ public class Compare {
 			case 2:
 				return nMinus1;
 			default:
+				//if n is not one of the base cases, iterate through using the formula
 				for(int i = 3; i <= n; i++) {
 					currentN = nMinus1 + 2 * nMinus2 - nMinus3 + 1;
+					//move the stored values backwards
 					nMinus3 = nMinus2;
 					nMinus2 = nMinus1;
 					nMinus1 = currentN;
+					//increment the counter as the basic operation has occured
 					_iterativeCount++;
 				}
+				//return the result
 				return currentN;
 		}
 	}
 	
+	/**
+	 * evaluates the algorithm using a recursive method
+	 * @param n the input size to evaluate the algorithm on
+	 * @return returns the result form evaluating the algorithm
+	 */
 	public static int Recursive(int n) {
+		//declare the initial facts
 		int nZero = -3;
 		int nOne = 3;
 		int nTwo = 2;
 		int currentN = n;
 		_recursiveCount++;
-
+		
+		//if n is one of the base cases return the base case
 		switch(n) {
 		case 0:
 			return nZero;
@@ -77,6 +106,7 @@ public class Compare {
 		case 2:
 			return nTwo;
 		default:
+			//otherwise use the recursive formula
 			currentN = Recursive(n-1) + 2 * Recursive(n-2) - Recursive(n-3) + 1;
 			return currentN;
 		}
--- Test.java
+++ Test.java
@@ -1,28 +1,37 @@
 /**
- * 
+ * Group Members: Neal Jamieson, James Gregory, Landdon Squiers
+ * Class: CSCI_3342_002
+ * Assignment: Exploration 1 
  */
 package exploration1;
 
 /**
- * @author Neal Jamieson, James Gregory, Landdon Squiers
- * Class: CSCI_3342_002
- * Assignment: Exploration 1
- *
+ * @author Neal Jamieson
+ * 
+ * This class takes the algorithms create in the Compare class and analyzes them based on given input sizes. The results are reported to the console including the number of times the basic
+ * operation occurred and the amount of time it took for the algorithm to complete.
  */
 public class Test {
 
 	/**
-	 * @param args
+	 * evaluates the given algorithms for specified input sizes
+	 * @param args command line arguments passed in at execution
 	 */
 	public static void main(String[] args) {
+		//Check n=2 [result should be 2]
 		checkIterative(2);
 		checkRecursive(2);
 		
+		//check n=10
 		checkIterative(10);
 		checkRecursive(10);
 		
 	}
 	
+	/**
+	 * runs the iterative algorithm and reports the number of times the basic operation occurred and how long it took for the algorithm to complete. Resets the counter variable when finished.
+	 * @param n the input size on which to evaluate the algorithm
+	 */
 	private static void checkIterative(int n) {
 		int result = 0;
 		
@@ -42,11 +51,17 @@ public class Test {
 		result = Compare.Iterative(n);
 		
 		stopTime = System.nanoTime();
-		long time = stopTime-startTime-overTime;		
+		long time = stopTime-startTime-overTime;
+		
+		//Display results and reset the counter
 		System.out.println(String.format("Iterative(%d) ran %d times and took %d nanoseconds to complete. The Result was %d", n, Compare.getIterativeCount(), time, result));
 		Compare.setIterativeCount(0);
 	}
 	
+	/**
+	 * runs the recursive algorithm and reports the number of times the basic operation occurred and how long it took for the algorithm to complete. Resets the counter variable when finished.
+	 * @param n the input size on which to evaluate the algorithm
+	 */
 	private static void checkRecursive(int n) {
 		int result = 0;
 		
@@ -67,7 +82,9 @@ public class Test {
 		
 		stopTime = System.nanoTime();
 		long time = stopTime-startTime-overTime;
-		System.out.println(String.format("Iterative(%d) ran %d times and took %d nanoseconds to complete. The Result was %d", n, Compare.getRecursiveCount(), time, result));
+		
+		//Display the results and reset the counter
+		System.out.println(String.format("Recursive(%d) ran %d times and took %d nanoseconds to complete. The Result was %d", n, Compare.getRecursiveCount(), time, result));
 		Compare.setRecursiveCount(0);
 	}
 
