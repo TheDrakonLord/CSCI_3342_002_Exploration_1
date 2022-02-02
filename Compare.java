8dd5eae88a8cdb33f828a26611add66e9e1fe0ab
Neal Jamieson <54950355+TheDrakonLord@users.noreply.github.com>
2022-02-02 14:36:15 -0600

    Full Functionality and initial tests

--- Compare.java
+++ Compare.java
@@ -4,9 +4,14 @@
 package exploration1;
 
 /**
- * @author nealc
+ * @author Neal Jamieson, James Gregory, Landdon Squiers
+ * Class: CSCI_3342_002
+ * Assignment: Exploration 1
+ * 
  *
  */
+
+
 public class Compare {
 	private static int _iterativeCount = 0;
 	private static int _recursiveCount = 0;
@@ -32,12 +37,48 @@ public class Compare {
 	}
 	
 	public static int Iterative(int n) {
+		_iterativeCount = 0;
+		int nMinus3 = -3;
+		int nMinus2 = 3;
+		int nMinus1 = 2;
+		int currentN = 0;
 		
-		return n;
-		
+		switch(n) {
+			case 0:
+				return nMinus3;
+			case 1:
+				return nMinus2;
+			case 2:
+				return nMinus1;
+			default:
+				for(int i = 3; i <= n; i++) {
+					currentN = nMinus1 + 2 * nMinus2 - nMinus3 + 1;
+					nMinus3 = nMinus2;
+					nMinus2 = nMinus1;
+					nMinus1 = currentN;
+					_iterativeCount++;
+				}
+				return currentN;
+		}
 	}
 	
 	public static int Recursive(int n) {
-		return n;
+		int nZero = -3;
+		int nOne = 3;
+		int nTwo = 2;
+		int currentN = n;
+		_recursiveCount++;
+
+		switch(n) {
+		case 0:
+			return nZero;
+		case 1:
+			return nOne;
+		case 2:
+			return nTwo;
+		default:
+			currentN = Recursive(n-1) + 2 * Recursive(n-2) - Recursive(n-3) + 1;
+			return currentN;
+		}
 	}
 }
--- Test.java
+++ Test.java
@@ -2,9 +2,11 @@
  * 
  */
 package exploration1;
-import exploration1.Compare;
+
 /**
- * @author nealc
+ * @author Neal Jamieson, James Gregory, Landdon Squiers
+ * Class: CSCI_3342_002
+ * Assignment: Exploration 1
  *
  */
 public class Test {
@@ -13,9 +15,60 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Compare _compare = new Compare();
+		checkIterative(2);
+		checkRecursive(2);
 		
+		checkIterative(10);
+		checkRecursive(10);
+		
+	}
+	
+	private static void checkIterative(int n) {
+		int result = 0;
+		
+		//System nanoTimer nanosecond resolution
+		long startTime = 0;
+		long stopTime = 0;
+		long overTime = 0;
+		//nano second -- clock resolution
+		startTime = System.nanoTime();
+		stopTime = System.nanoTime();
+		overTime = stopTime - startTime;
+		
+		//Start timer for execution timing
+		startTime = System.nanoTime();
+		
+		//Execute algorithm to be tested
+		result = Compare.Iterative(n);
+		
+		stopTime = System.nanoTime();
+		long time = stopTime-startTime-overTime;		
+		System.out.println(String.format("Iterative(%d) ran %d times and took %d nanoseconds to complete. The Result was %d", n, Compare.getIterativeCount(), time, result));
+		Compare.setIterativeCount(0);
+	}
+	
+	private static void checkRecursive(int n) {
+		int result = 0;
+		
+		//System nanoTimer nanosecond resolution
+		long startTime = 0;
+		long stopTime = 0;
+		long overTime = 0;
+		//nano second -- clock resolution
+		startTime = System.nanoTime();
+		stopTime = System.nanoTime();
+		overTime = stopTime - startTime;
+		
+		//Start timer for execution timing
+		startTime = System.nanoTime();
+		
+		//Execute algorithm to be tested
+		result = Compare.Recursive(n);
+		
+		stopTime = System.nanoTime();
+		long time = stopTime-startTime-overTime;
+		System.out.println(String.format("Iterative(%d) ran %d times and took %d nanoseconds to complete. The Result was %d", n, Compare.getRecursiveCount(), time, result));
+		Compare.setRecursiveCount(0);
 	}
 
 }
